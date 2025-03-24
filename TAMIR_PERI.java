5e8fe3dbb60b59fbcd8e9f8384b02873bca8b1f3
Tamirperi <tamirperi15@gmail.com>
2025-03-24 22:37:58 +0200

    helloo

---
+++ TAMIR_PERI.java
@@ -0,0 +1,8 @@
+
+public class TAMIR_PERI{
+        public static void main(String[] args) {
+               for (int i=1;i<=10; i++) {
+                        System.out.println("Tamir Peri ..."+i);
+               }
+        }
+}
