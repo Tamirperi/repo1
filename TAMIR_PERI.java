49cb4b0a69de6f51fa0ab656c31ad0e6f39bae42
Tamirperi <tamirperi15@gmail.com>
2025-03-27 20:26:32 +0200

    swap names

--- TAMIR_PERI.java
+++ TAMIR_PERI.java
@@ -2,7 +2,7 @@
 public class TAMIR_PERI{
         public static void main(String[] args) {
                for (int i=1;i<=10; i++) {
-                        System.out.println("Tamir Peri ..."+i);
+                        System.out.println("PERI TAMIR (after swap) ..."+i);
                }
         }
 }
